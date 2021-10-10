4412dcfd615721c4b70902a7d2f3e788361b3268
Kaorisati <kaorisati@gmail.com>
2021-10-09 23:12:28 -0300

    Concesionaria_Autos_Entregable_VersFinal

--- concesionaria.js
+++ concesionaria.js
@@ -2,9 +2,9 @@ const autos=require("./autos");
 
 
 let concesionaria = {
-   autos: autos,
+ autos: autos,
  
-   buscarAuto: function(pat){
+ buscarAuto: function(pat){
    let pos=autos.map(e=>e.patente).indexOf(pat);
    return autos[pos]
    },
@@ -18,29 +18,37 @@ let concesionaria = {
        }
       }*/
    
-   venderAuto:function(pat){
-       
+ venderAuto:function(pat){  
         if(this.buscarAuto(pat)!=null){
             this.buscarAuto(pat).vendido=true;
         }     
         
-      },
+    },
 
-   autosParaLaVenta: function(){
+ autosParaLaVenta: function(){
     return autos.filter(au=>au.vendido==false)
- },
+    },
  autosNuevos: function(){
     return this.autosParaLaVenta().filter(au=>au.km<100)
- },
+    },
  listaDeVentas: function(){
     return autos.filter(au=>au.vendido==true).map(au=>au.precio)
- },
+    },
  totalDeVentas: function(){
     if(this.listaDeVentas().length>0){
         return this.listaDeVentas().reduce((a,b)=>a+b )}
-        else{ return 0 }}
+        else{ return 0 }
+    },
+ puedeComprar: function(auto,persona){
+    //let auto=this.buscarAuto(pat);
+    if(auto.precio<persona.capacidadDePagoTotal & (auto.precio/auto.cuotas)<persona.capacidadDePagoEnCuotas){
+         return true
+      }else {return false}
+    },
+ 
 }
 
 concesionaria.venderAuto("APL123");
 
 console.log(concesionaria.totalDeVentas())
+console.log(concesionaria.autosQuePuedeComprar({capacidadDePagoEnCuotas: 30000, capacidadDePagoTotal: 1000000}))
